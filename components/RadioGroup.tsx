b447a8d366b5734193cba16b6be146fcb1a8a8db
jiamengda <jiamengda@jyd.com.cn>
2024-05-07 17:28:36 +0800

    feat: 修改组件

--- components/RadioGroup.tsx
+++ components/RadioGroup.tsx
@@ -49,7 +49,7 @@
 //         }else {
 //             setRadioDefaultValue(`${lowestPeopleNum},${highestPeopleNum}`);
 //         }
-        
+
 //     },[value])
 
 //     // useeffect监听用餐人数值变化
@@ -157,64 +157,64 @@
 
 // export default DinnerCount;
 
-const allGroups = [
-	[
-        {
-            "itemKey": "1709017720839550GROUP",
-            "title": "哦哦哦",
-            "selectNum": 1,
-            "fromNum": 1,
-            "setMealContents": [{
-                "itemKey": "1709017720839777MEAL",
-                "title": "看看",
-                "count": 1,
-                "price": 1,
-                "subContents": []
-            }]
-	    },
-        {
-            "itemKey": "1709017720839550GROUP",
-            "title": "我是多个",
-            "selectNum": 1,
-            "fromNum": 1,
-            "setMealContents": [{
-                "itemKey": "1709017720839777MEAL",
-                "title": "看看2",
-                "count": 1,
-                "price": 1000,
-                "subContents": []
-            }]
-	    },
-    ],
-	[
-        {
-            "itemKey": "1709017757129745GROUP",
-            "title": "哦哦",
-            "selectNum": 1,
-            "fromNum": 1,
-            "setMealContents": [{
-                "itemKey": "1709017757129279MEAL",
-                "title": "哦哦",
-                "count": 1,
-                "price": 1,
-                "subContents": []
-            }]
-        },
-        {
-            "itemKey": "1709017757129745GROUP",
-            "title": "哦哦",
-            "selectNum": 1,
-            "fromNum": 1,
-            "setMealContents": [{
-                "itemKey": "1709017757129279MEAL",
-                "title": "哦哦",
-                "count": 1,
-                "price": 1,
-                "subContents": []
-            }]
-        }
-    ],
-]
+// const allGroups = [
+// 	[
+//         {
+//             "itemKey": "1709017720839550GROUP",
+//             "title": "哦哦哦",
+//             "selectNum": 1,
+//             "fromNum": 1,
+//             "setMealContents": [{
+//                 "itemKey": "1709017720839777MEAL",
+//                 "title": "看看",
+//                 "count": 1,
+//                 "price": 1,
+//                 "subContents": []
+//             }]
+// 	    },
+//         {
+//             "itemKey": "1709017720839550GROUP",
+//             "title": "我是多个",
+//             "selectNum": 1,
+//             "fromNum": 1,
+//             "setMealContents": [{
+//                 "itemKey": "1709017720839777MEAL",
+//                 "title": "看看2",
+//                 "count": 1,
+//                 "price": 1000,
+//                 "subContents": []
+//             }]
+// 	    },
+//     ],
+// 	[
+//         {
+//             "itemKey": "1709017757129745GROUP",
+//             "title": "哦哦",
+//             "selectNum": 1,
+//             "fromNum": 1,
+//             "setMealContents": [{
+//                 "itemKey": "1709017757129279MEAL",
+//                 "title": "哦哦",
+//                 "count": 1,
+//                 "price": 1,
+//                 "subContents": []
+//             }]
+//         },
+//         {
+//             "itemKey": "1709017757129745GROUP",
+//             "title": "哦哦",
+//             "selectNum": 1,
+//             "fromNum": 1,
+//             "setMealContents": [{
+//                 "itemKey": "1709017757129279MEAL",
+//                 "title": "哦哦",
+//                 "count": 1,
+//                 "price": 1,
+//                 "subContents": []
+//             }]
+//         }
+//     ],
+// ]
 
 // interface Type {
 //     currentIndex: number;
@@ -226,23 +226,23 @@ const allGroups = [
 // }
 
 // 校验所有skulist数据中的每一项数据的group的price单价是否全部为空，如果全部为空，则给该项数据加一个字段标识是全部为空
-let emptyIndexSet = new Set()
-const setMealContentsTemp = allGroups.reduce((pre, subGroup,currentIndex)=>{
-    const setMealContentsItem = subGroup.map(({setMealContents = []}) => 
-        setMealContents.map(item => {
-            const {price} = item
-            const isEmpty = !price;
-            isEmpty && emptyIndexSet.add(currentIndex)
-            return item
-        })
-    ).flat()
-    return [...pre, ...setMealContentsItem];
-},[])
-const allEmpty = setMealContentsTemp.every(({price}) => !price)
-const allHas = setMealContentsTemp.every(({price}) => !!price)
-if(!allEmpty && !allHas) {
-    // newErrMsgs.group[[...emptyIndexSet][0]] = '方案搭配中的所有单价必填'
-}
+// let emptyIndexSet = new Set()
+// const setMealContentsTemp = allGroups.reduce((pre, subGroup,currentIndex)=>{
+//     const setMealContentsItem = subGroup.map(({setMealContents = []}) =>
+//         setMealContents.map(item => {
+//             const {price} = item
+//             const isEmpty = !price;
+//             isEmpty && emptyIndexSet.add(currentIndex)
+//             return item
+//         })
+//     ).flat()
+//     return [...pre, ...setMealContentsItem];
+// },[])
+// const allEmpty = setMealContentsTemp.every(({price}) => !price)
+// const allHas = setMealContentsTemp.every(({price}) => !!price)
+// if(!allEmpty && !allHas) {
+//     // newErrMsgs.group[[...emptyIndexSet][0]] = '方案搭配中的所有单价必填'
+// }
 
 // function floatArr (obj = {}, pIndex, emptyIndexSet = new Set()) {
 //     if(Array.isArray(obj)) {
@@ -262,9 +262,6 @@ if(!allEmpty && !allHas) {
 
 // console.log([...floatArr(allGroups)])
 
-
-
-
 // let groupIndex = 0;
 // const isAllPricePass = allGroups.every((fitem, index) => {
 //     const allEmpty = fitem.every((item) => item.setMealContents.length > 0 && item.setMealContents.every((meal) => !meal.price));
@@ -277,3 +274,120 @@ if(!allEmpty && !allHas) {
 // });
 // console.log('allGroups-----------------', isAllPricePass);
 // !isAllPricePass && ( newErrMsgs.group[groupIndex] = '方案搭配中的所有单价必填')
+
+// const isEditOrAddShow = newSkuList?.length == 1 || (!isOnlyEdit && !newSkuList?.length);
+
+let data = [
+  {
+    categoryId: 10000000,
+    categoryName: "美食",
+    categoryPid: 0,
+    hierarchy: 1,
+    sortNum: 1,
+    categoryDesc: "美食",
+    isLeaf: false,
+    categoryStatus: 100,
+    createTime: 1695281501122,
+    updateTime: 1706496739436,
+    childCategory: [
+      {
+        categoryId: 10100000,
+        categoryName: "北京菜",
+        categoryPid: 10000000,
+        hierarchy: 2,
+        sortNum: 1,
+        categoryDesc: "美食-北京菜",
+        isLeaf: false,
+        categoryStatus: 100,
+        createTime: 1695281501122,
+        updateTime: 1706496739441,
+        childCategory: [
+          {
+            categoryId: 10100001,
+            categoryName: "烤鸭",
+            categoryPid: 10100000,
+            hierarchy: 3,
+            sortNum: 1,
+            categoryDesc: "美食-北京菜-烤鸭",
+            isLeaf: true,
+            categoryStatus: 100,
+            createTime: 1695281501122,
+            updateTime: 1706496739446,
+            childCategory: null,
+            disable: true,
+            disableReason: "kdmkfmdkmfkmk",
+            disableType: 0,
+          },
+          {
+            categoryId: 10100002,
+            categoryName: "北京菜",
+            categoryPid: 10100000,
+            hierarchy: 3,
+            sortNum: 2,
+            categoryDesc: "美食-北京菜-北京菜",
+            isLeaf: true,
+            categoryStatus: 100,
+            createTime: 1695281501122,
+            updateTime: 1706496739447,
+            childCategory: null,
+            disable: false,
+            disableReason: null,
+            disableType: 0,
+          },
+        ],
+        disable: false,
+        disableReason: null,
+        disableType: 0,
+      },
+      {
+        categoryId: 10200000,
+        categoryName: "地方菜",
+        categoryPid: 10000000,
+        hierarchy: 2,
+        sortNum: 2,
+        categoryDesc: "美食-地方菜",
+        isLeaf: false,
+        categoryStatus: 100,
+        createTime: 1695281501122,
+        updateTime: 1706496739554,
+        childCategory: [
+          {
+            categoryId: 10200001,
+            categoryName: "川菜",
+            categoryPid: 10200000,
+            hierarchy: 3,
+            sortNum: 1,
+            categoryDesc: "美食-地方菜-川菜",
+            isLeaf: true,
+            categoryStatus: 100,
+            createTime: 1695281501122,
+            updateTime: 1706496739558,
+            childCategory: null,
+            disable: false,
+            disableReason: null,
+            disableType: 0,
+          },
+          {
+            categoryId: 10200002,
+            categoryName: "东北菜",
+            categoryPid: 10200000,
+            hierarchy: 3,
+            sortNum: 2,
+            categoryDesc: "美食-地方菜-东北菜",
+            isLeaf: true,
+            categoryStatus: 100,
+            createTime: 1695281501122,
+            updateTime: 1706496739560,
+            childCategory: null,
+            disable: false,
+            disableReason: null,
+            disableType: 0,
+          },
+        ],
+        disable: false,
+        disableReason: null,
+        disableType: 0,
+      },
+    ],
+  },
+];
